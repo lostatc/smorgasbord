f70504d4137a86ac1db762a83853044c7f6ed8ef
Wren Powell <wrenp@duck.com>
2024-07-10 22:57:48 -0400

    Rewrite homepage

--- client/src/views/HomeView.vue
+++ client/src/views/HomeView.vue
@@ -4,22 +4,36 @@ import { RouterLink } from "vue-router";
 
 <template>
   <main>
-    <h1 class="site-title">The Relationship Smorgasbord</h1>
-    <RouterLink to="/start" class="start-button nav-link">Start the discussion</RouterLink>
+    <h1 class="site-title">Negotiate your relationships</h1>
+    <RouterLink to="/start" class="start-button nav-link">Start a discussion</RouterLink>
     <p class="warning">🚧 This site is under construction 🚧</p>
     <p>
-      The relationship smorgasbord is a tool for negotiating relationships. Each of you will pick
-      and choose what you want out of your relationship, and once you're both done, you can compare
-      answers to make sure you're on the same page.
+      This is a tool for negotiating relationships. Each of you pick and choose what you want out of
+      your relationship from a menu of options, and once you're both done, you can compare answers
+      to see if you're on the same page or not.
+    </p>
+    <p>
+      The purpose of this tool is to start a discussion, not to replace one! Once you compare
+      answers, take the time to ask each other clarifying questions.
     </p>
     <p>
       You'll only see the answers that you agree on. If one of you selects "No" to a particular
-      question, you won't see how the other person responded.
+      question, you won't see how the other person responded. Don't be afraid to say "Yes" to
+      something just because you're worried the other person will say "No"!
     </p>
     <p>
-      The relationship smorgasbord isn't just for romantic relationships! Use it with your friends,
-      your platonic partners, your play partners, or anyone else you want to build a healthier
-      relationship with.
+      This tool isn't just for romantic relationships! Use it with your friends, your platonic
+      partners, your play partners, or anyone else you want to build a healthier relationship with.
+    </p>
+    <p>
+      You'll often see this exercise referred to as the "relationship smorgasbord," referring to the
+      way you can pick and choose what you want from a buffet of options. The concept comes from the
+      <a
+        href="https://theanarchistlibrary.org/library/andie-nordgren-the-short-instructional-manifesto-for-relationship-anarchy"
+        target="_blank"
+        >relationship anarchy</a
+      >
+      community.
     </p>
   </main>
 </template>
