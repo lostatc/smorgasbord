9f85c95d3e66cc55e0581fd38619cf3712f986ab
Wren Powell <wrenp@duck.com>
2024-07-14 06:52:20 -0400

    Use Naive UI blockquote

--- client/src/components/QuestionAnswer.vue
+++ client/src/components/QuestionAnswer.vue
@@ -1,5 +1,6 @@
 <script setup lang="ts">
 import { type AnswerType, humanReadableAnswer } from "@/types";
+import { NBlockquote } from "naive-ui";
 
 const props = defineProps<{
   playerName: string;
@@ -12,13 +13,8 @@ const props = defineProps<{
   <div class="question-answer">
     <strong>{{ props.playerName }}</strong
     >: {{ humanReadableAnswer(props.questionAnswer) }}
-    <blockquote v-if="props.notes">{{ props.notes }}</blockquote>
+    <n-blockquote v-if="props.notes" align="justify">{{ props.notes }}</n-blockquote>
   </div>
 </template>
 
-<style scoped>
-blockquote {
-  margin-bottom: 0;
-  text-align: justify;
-}
-</style>
+<style scoped></style>
