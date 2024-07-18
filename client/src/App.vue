3db4230d9110089c990dfa576a9a6d5e8afe7b0c
Wren Powell <wrenp@duck.com>
2024-07-18 17:27:25 -0400

    Add Bootstrap to Vite

--- client/src/App.vue
+++ client/src/App.vue
@@ -1,43 +1,14 @@
 <script setup lang="ts">
-import {
-  NConfigProvider,
-  NGlobalStyle,
-  NDialogProvider,
-  darkTheme,
-  useOsTheme,
-  type GlobalThemeOverrides,
-  NMessageProvider,
-} from "naive-ui";
-import { computed } from "vue";
+import { NDialogProvider, NMessageProvider } from "naive-ui";
 import TopNav from "@/components/TopNav.vue";
-
-const themeRef = useOsTheme();
-const theme = computed(() => {
-  return themeRef.value === "dark" ? darkTheme : undefined;
-});
-
-const themeOverrides: GlobalThemeOverrides = {
-  common: {
-    fontSize: "12pt",
-    fontSizeMini: "8pt",
-    fontSizeTiny: "10pt",
-    fontSizeSmall: "10pt",
-    fontSizeMedium: "12pt",
-    fontSizeLarge: "14pt",
-    fontSizeHuge: "16pt",
-  },
-};
 </script>
 
 <template>
-  <n-config-provider :theme="theme" :theme-overrides="themeOverrides">
-    <n-dialog-provider>
-      <n-message-provider keep-alive-on-hover placement="bottom">
-        <TopNav />
-        <n-global-style />
-      </n-message-provider>
-    </n-dialog-provider>
-  </n-config-provider>
+  <n-dialog-provider>
+    <n-message-provider keep-alive-on-hover placement="bottom">
+      <TopNav />
+    </n-message-provider>
+  </n-dialog-provider>
 </template>
 
 <style scoped></style>
--- client/src/main.ts
+++ client/src/main.ts
@@ -1,4 +1,5 @@
-import "./assets/main.css";
+import "./assets/main.scss";
+import * as bootstrap from "bootstrap";
 
 import { createApp } from "vue";
 import App from "./App.vue";
