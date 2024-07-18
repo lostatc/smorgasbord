26dc5e443f9854e4ba9da37e9fd30b3ab1df3732
Wren Powell <wrenp@duck.com>
2024-07-18 05:59:29 -0400

    Show popup message on link copy

--- client/src/components/CopyButton.vue
+++ client/src/components/CopyButton.vue
@@ -1,30 +1,21 @@
 <script setup lang="ts">
-import { ref } from "vue";
-import { NButton, NText, NFlex } from "naive-ui";
+import { NButton, useMessage } from "naive-ui";
+
+const message = useMessage();
 
 const props = defineProps<{
   text: string;
   link: string;
 }>();
 
-const isCopied = ref(false);
-
 const copyLink = () => {
   navigator.clipboard.writeText(props.link);
-
-  isCopied.value = true;
-
-  setTimeout(() => {
-    isCopied.value = false;
-  }, 1500);
+  message.success("Link copied!");
 };
 </script>
 
 <template>
-  <n-flex size="medium" align="baseline">
-    <n-button @click="copyLink">{{ props.text }}</n-button>
-    <n-text depth="2" v-if="isCopied">Copied!</n-text>
-  </n-flex>
+  <n-button @click="copyLink">{{ props.text }}</n-button>
 </template>
 
 <style scoped></style>
