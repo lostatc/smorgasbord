50d1ef365b83d43f858f089a0aa9c1c4e4b1a996
Wren Powell <wrenp@duck.com>
2024-07-18 17:41:57 -0400

    WIP convert start page

--- client/src/components/NameInput.vue
+++ client/src/components/NameInput.vue
@@ -1,44 +1,23 @@
 <script setup lang="ts">
-import { NInput, NFormItem } from "naive-ui";
-
 const model = defineModel<string>();
 
 const props = defineProps<{
   id: string;
   label: string;
-  path: string;
 }>();
 </script>
 
 <template>
-  <!--
-    Naive UI doesn't do accessibility properly with form inputs. The form label
-    isn't linked to the form input by default.
-
-    https://github.com/tusen-ai/naive-ui/issues/4598
-  -->
-  <n-form-item
-    :label="props.label"
-    :path="path"
-    class="name-input"
-    :label-props="{ for: `name-input-${props.id}` }"
-  >
-    <!--
-      Naive UI doesn't provide an accessible way of marking a form input as
-      required.
-    -->
-    <n-input
+  <div class="mb-3">
+    <label :for="`name-input-${props.id}`" class="form-label">{{ props.label }}</label>
+    <input
+      :id="`name-input-${props.id}`"
+      class="form-control"
       type="text"
-      placeholder=""
-      v-model:value="model"
-      :input-props="{ id: `name-input-${props.id}`, ['aria-required']: true }"
+      v-model="model"
+      required
     />
-  </n-form-item>
+  </div>
 </template>
 
-<style scoped>
-.name-input {
-  display: flex;
-  flex-direction: column;
-}
-</style>
+<style scoped></style>
--- client/src/views/StartView.vue
+++ client/src/views/StartView.vue
@@ -3,10 +3,9 @@ import { ref } from "vue";
 import { sessionsEndpoint } from "@/api";
 import NameInput from "@/components/NameInput.vue";
 import { useRouter } from "vue-router";
-import { type FormInst, NButton, NForm, NUl, NLi, NFlex } from "naive-ui";
 import { useMessage } from "@/message";
 
-const formRef = ref<FormInst>();
+const formRef = ref<HTMLFormElement>();
 const names = ref({ sender: "", recipient: "" });
 
 const router = useRouter();
@@ -64,10 +63,10 @@ const startSession = async () => {
 
 <template>
   <main aria-labelledby="main-heading">
-    <h1 id="main-heading" class="title">Start a new discussion</h1>
-    <n-flex size="large" justify="space-around">
+    <h1 id="main-heading" class="text-center mb-4">Start a new discussion</h1>
+    <div class="d-flex justify-content-around">
       <div class="form">
-        <n-form aria-labelledby="main-heading" ref="formRef" :rules="rules" :model="names">
+        <form aria-labelledby="main-heading" ref="formRef" :rules="rules" :model="names">
           <name-input id="sender" label="Your name" path="sender" v-model="names.sender" />
           <name-input
             id="recipient"
@@ -75,51 +74,46 @@ const startSession = async () => {
             path="recipient"
             v-model="names.recipient"
           />
-          <n-button @click="startSession">Start</n-button>
-        </n-form>
+          <button @click="startSession" class="btn btn-primary">Start</button>
+        </form>
       </div>
       <aside class="aside">
-        <n-ul>
-          <n-li>
+        <ul>
+          <li>
             <p>You won't be able to see each other's answers until you're both done answering.</p>
-          </n-li>
-          <n-li>
+          </li>
+          <li>
             <p>
               You'll only see the answers that you agree on. If one of you selects "No" to a
               particular question, you won't see how the other person responded. Don't be afraid to
               say yes to something just because you're worried the other person will say no!
             </p>
-          </n-li>
-          <n-li>
+          </li>
+          <li>
             <p>
               If you want to change your answers, you'll both have the opportunity to go back and
               make changes before you can compare answers again.
             </p>
-          </n-li>
-          <n-li>
+          </li>
+          <li>
             <p>
               The purpose of this tool is to start a discussion, not to replace one! Once you
               compare answers, take the time to ask each other clarifying questions.
             </p>
-          </n-li>
-          <n-li>
+          </li>
+          <li>
             <p>
               You can delete your data from this website at any time. It will auto-delete after 7
               days.
             </p>
-          </n-li>
-        </n-ul>
+          </li>
+        </ul>
       </aside>
-    </n-flex>
+    </div>
   </main>
 </template>
 
 <style scoped>
-.title {
-  text-align: center;
-  margin-bottom: 2rem;
-}
-
 .form {
   flex: 0 1 20rem;
 }
