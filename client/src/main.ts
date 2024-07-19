686acd517f6442f0d5617322f1abb6b0d2257067
Wren Powell <wrenp@duck.com>
2024-07-19 06:21:28 -0400

    Change primary color

--- client/src/main.ts
+++ client/src/main.ts
@@ -5,13 +5,20 @@ import App from "./App.vue";
 import router from "./router";
 import PrimeVue from "primevue/config";
 import Aura from "@primevue/themes/aura";
+import { definePreset, palette } from "@primevue/themes";
 
 const app = createApp(App);
+
+const preset = definePreset(Aura, {
+  semantic: {
+    primary: palette("#63e2b7"),
+  },
+});
 
 app.use(router);
 app.use(PrimeVue, {
   theme: {
-    preset: Aura,
+    preset,
   },
 });
 
